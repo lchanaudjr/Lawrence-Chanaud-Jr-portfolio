65ef62737cd32fa4a0d5cbc00be428e0bbc9ddee
Lawrence Chanaud Jr <lchanaudjr@yahoo.com>
2024-08-12 22:58:44 -0400

    Finished 1.0

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <div className="w-full py-10 h-auto border-b-[1px] border-b-black ">
       <div className="w-full h-full flex inline justify-center">
-        <img className="w-32 mr-3 inline" src={logo} alt="logo" />
+        <img className="w-20 h-20 mr-3 inline" src={logo} alt="logo" />
         <div className="flex justify-between">
           <span className="bannerIcon">
             <FaLinkedinIn />
